0e674da56090edb0163e7b31407a4175d323d537
Andrei Baicus <baicusandrei@me.com>
2019-09-19 11:55:37 +0300

    fix: style loading priority [fixes Codeinwp/neve#751]

--- functions.php
+++ functions.php
@@ -11,4 +11,4 @@ if ( ! function_exists( 'neve_child_load_css' ) ):
 		wp_enqueue_style( 'neve-child-style', trailingslashit( get_stylesheet_directory_uri() ) . 'style.css', array( 'neve-style' ), NEVE_VERSION );
 	}
 endif;
-add_action( 'wp_enqueue_scripts', 'neve_child_load_css', 10 );
+add_action( 'wp_enqueue_scripts', 'neve_child_load_css', 20 );
